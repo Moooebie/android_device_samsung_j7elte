1d45233ebbaf0f29e3d5e92f3f34e3b020a1b3c6
FlominatorGD <github.flominatorgd@gmail.com>
2021-05-22 16:26:32 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/lineage_j7elte.mk
+    $(LOCAL_PATH)/havoc_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
     havoc_j7elte-user \
