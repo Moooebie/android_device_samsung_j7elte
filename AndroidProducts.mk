a93346979460b72c8c1d6c8f3cafe7ba1c80e7b2
Moebie Woe <mcpro233@gmail.com>
2021-12-12 16:32:21 -0400

    havoc -> octavi

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/havoc_j7elte.mk
+    $(LOCAL_PATH)/octavi_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_j7elte-user \
-    havoc_j7elte-userdebug \
-    havoc_j7elte-eng
+    octavi_j7elte-user \
+    octavi_j7elte-userdebug \
+    octavi_j7elte-eng
